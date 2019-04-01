18d231f2354ebc88a339582f4ce3451f607398ef
city <city_wangyi@163.com>
2019-04-01 11:18:20 +0800

    添加图片处理工具类

--- src/main/java/com/huntto/controller/TestController.java
+++ src/main/java/com/huntto/controller/TestController.java
@@ -9,16 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.cloud.demo.Utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.huntto.dao.OlexamCyryJbxxDao;
 import com.huntto.entity.CyryVo;
+import com.huntto.entity.face.FaceDetectResult;
 import com.huntto.service.CyryService;
 import com.huntto.service.OlexamCyryJbxxService;
+import com.huntto.util.FaceRecognUtil;
 import com.huntto.util.JsonUtil;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Description: 类描述 <br/>
@@ -30,11 +34,23 @@ import io.swagger.annotations.ApiOperation;
  * Create_time 2018/12/8 9:16<br/>
  */
 //@Api("健康证-测试接口")
+@Slf4j
 @RestController
 public class TestController {
 	@Autowired
 	private OlexamCyryJbxxDao olexamCyryJbxxDao;
+	@Autowired
+	private FaceRecognUtil faceRecognUtil;
 	
+	@RequestMapping(value = { "/testP" }, method = RequestMethod.POST)
+	public String test() throws Exception {
+		byte[] jpegData = Utils.loadFile(Utils.PICTURE_ROOT + "620502199207234346.jpg");
+		log.info("jpegData length: "+jpegData.length);
+    	String img1 = Base64.encodeBase64String(jpegData);
+    	log.info("img1 length: "+img1.length());
+		FaceDetectResult fDetectResult = faceRecognUtil.faceDetect(img1);
+		return fDetectResult.toString();
+	}
 	@Autowired
 	private OlexamCyryJbxxService olexamCyryJbxxService;
 	public static void main(String[] args) throws Exception {
